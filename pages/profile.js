61104e9a99b734bc214bda296d3932e93d6b7402
fruitbox12 <dylanwong007@gmail.com>
2023-01-24 01:59:42 -0800

    Update profile.js

--- pages/profile.js
+++ pages/profile.js
@@ -5,9 +5,9 @@ const Profile = () => {
   const user = useUser({ redirectTo: '/login' })
 
   return (
-    <Layout>
+    <>
     <iframe src="https://aad4-45-50-98-105.ngrok.io/" height="100%" width="100%" frameborder="0">Your browser doesnot support iframes<a href="https://aad4-45-50-98-105.ngrok.io/"> click here to view the page directly. </a></iframe>
-    </Layout>
+  </>
   )
 }
 
